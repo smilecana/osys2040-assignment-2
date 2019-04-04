8f739b282279079d1c0aff05126a9045898868e5
smilecana <smilecana@gmail.com>
2019-04-04 20:44:29 -0300

    modified database

--- src/utils/PostgresUtil.js
+++ src/utils/PostgresUtil.js
@@ -1,13 +1,13 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  //connectionString: process.env.DATABASE_URL,
-  // ssl: true,
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'password',
-  port: '5432',
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+  // user: 'postgres',
+  // host: 'localhost',
+  // database: 'postgres',
+  // password: 'password',
+  // port: '5432',
 
 })
 
