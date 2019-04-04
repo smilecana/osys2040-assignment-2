c7438436905325a4e127c92e78c8c625cb0b6b8f
smilecana <smilecana@gmail.com>
2019-04-04 17:00:07 -0300

    commit Test

--- src/utils/PostgresUtil.js
+++ src/utils/PostgresUtil.js
@@ -1,8 +1,14 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  //connectionString: process.env.DATABASE_URL,
   // ssl: true,
+  user: 'postgresql',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'password',
+  port: '5432',
+
 })
 
 module.exports = {
