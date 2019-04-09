99aa5f4702cc58bb05062b649d0bedadc00c7e0b
smilecana <smilecana@gmail.com>
2019-04-09 00:58:10 -0300

    chage db connection info

--- src/utils/PostgresUtil.js
+++ src/utils/PostgresUtil.js
@@ -1,13 +1,13 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
- // connectionString: process.env.DATABASE_URL,
- // ssl: true,
-  user: 'smilecana',
-  host: 'localhost',
-  database: 'chat',
-  password: 'password',
-  port: '5432',
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+ // user: 'smilecana',
+ // host: 'localhost',
+ // database: 'chat',
+ // password: 'password',
+ // port: '5432',
 
 })
 
